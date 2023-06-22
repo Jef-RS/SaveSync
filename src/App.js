3e805617fdc5eea49cc4e67dce312f2523c60f84
Anderson B.O.B <55605767+eusouanderson@users.noreply.github.com>
2023-06-22 11:28:44 -0300

    "update"

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import { ipcRenderer } from 'electron';
+
+function App() {
+  const handleOpenDirectory = () => {
+    ipcRenderer.send('select-directory');
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleOpenDirectory}>Abrir Diretório</button>
+    </div>
+  );
+}
+
+export default App;
