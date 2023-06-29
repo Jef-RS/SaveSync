9771bdf45e0da607e247a8b6b3bcaa71efd9cd0f
Anderson <eusouanderson@outlook.com>
2023-06-29 10:27:35 -0300

    "update"

--- Backend/firebase/main.py
+++ Backend/firebase/main.py
@@ -1,34 +1,30 @@
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db
+from firebase_admin import firestore
 import os
 
 diretório = os.path.dirname(__file__)
 name_arq = os.path.basename(diretório)
 diretório_raiz = diretório[:-8]
 
-# Cria a conexão com o banco de dados
+
 print(f'Diretório do Banco de dados {diretório_raiz}')
 
-# Inicializar o SDK do Firebase
+# Initialize the Firebase app
 cred = credentials.Certificate(f"{diretório_raiz}firebase/firebase.json")
 firebase_admin.initialize_app(cred)
 
-# Obtenha a URL do banco de dados de uma variável de ambiente
-database_url = os.environ.get('FIREBASE_DATABASE_URL')
+# Get a Firestore client
+db = firestore.client()
 
-if database_url:
-    # Referência para o nó "dados" no banco de dados
-    ref = db.reference(database_url)
+# Specify the collection and document data
+collection_name = "users"
+document_data = {
+    "name": "John Doe",
+    "email": "johndoe@example.com",
+    "age": 30
+}
 
-    # Dados a serem salvos
-    dados = {
-        'username': 'admin',
-    }
-
-    # Salvar os dados no banco de dados
-    ref.set(dados)
-else:
-    print("A variável de ambiente FIREBASE_DATABASE_URL não está definida.")
-
+# Add the document to the collection
+db.collection(collection_name).add(document_data)
 
