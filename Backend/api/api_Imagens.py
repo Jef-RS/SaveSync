cadc631ad78a40fb4e8cc748ed991d893c6d9d93
Anderson <eusouanderson@outlook.com>
2023-07-08 12:00:43 -0300

    update

--- Backend/api/api_Imagens.py
+++ Backend/api/api_Imagens.py
@@ -1,28 +1,50 @@
+import re
 import requests
-
-Game_ID = 252490
-steam_game_name = f"https://steamdb.info/app/{Game_ID}/"
+from bs4 import BeautifulSoup
 
 
-steam_URL = f'https://cdn.akamai.steamstatic.com/steam/apps/{Game_ID}/header.jpg?t=1675801903'
+
+Game_ID = 1593500
+steam_game_name = f"https://store.steampowered.com/app/{Game_ID}"
 
 
+steam_URL = f'https://cdn.akamai.steamstatic.com/steam/apps/{Game_ID}/header.jpg'
+
+
+responseName = requests.get(steam_game_name)
 response = requests.get(steam_URL)
+html_content = responseName.text
+
+soup = BeautifulSoup(html_content, "html.parser")
+div_element = str(soup.find(id="appHubAppName"))
+
+
+html_string = div_element
+
+pattern = r'<div class="apphub_AppName" id="appHubAppName">(.+?)</div>'
+
+match = re.search(pattern, html_string)
+
+if match:
+    title = match.group(1)
+    print(title)  
+else:
+    print("Título não encontrado.")
 
 def append_image():
     """
         A function that appends an image to 'Imagens_dos_games/header.jpg' if the response status code is 200.
     """
-
+    
     if response.status_code == 200:
         print('OK')
         try:
-            
-            with open('header.jpg', 'wb') as f:
+
+            with open(f'Backend/routes/static/images/api_Imagens/header.jpg', 'wb') as f:
                 f.write(response.content)
         except Exception as e:
             print(e)
     else:
         print('Game id not found')
         
-#append_image()
+append_image()
